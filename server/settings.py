18d4fb203ab7ade63b413a8bab1784394e2b6a01
Bedoy <carlos.bedoy@gmail.com>
2017-12-22 21:23:42 -0600

    First configuration

--- server/settings.py
+++ server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'server',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'you_are_server.db'),
     }
 }
 
